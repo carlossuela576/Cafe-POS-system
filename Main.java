f0d9714ef5b4eb6ea123f0fe9f5fc32b6d638b12
Carlos Miguel Marquez Suela <126050848+carlossuela576@users.noreply.github.com>
2025-06-12 21:08:46 +0700

    TO-DO: fix displayOrder()

--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         itmPrc itemsAndPrices = new itmPrc();
+        Orditf orderInterface = new Orditf();
 
         itemsAndPrices.putDataItem();
         itemsAndPrices.putDataPrice();
 
-        itemsAndPrices.displayMenu();
+        do{
+            itemsAndPrices.displayMenu();
+            orderInterface.getOrder();
+            orderInterface.displayOrder();
+        }
+        while(true);
     }
 }
--- Orditf.java
+++ Orditf.java
@@ -1,6 +1,9 @@
+import java.util.InputMismatchException;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Orditf {
+    Scanner sc = new Scanner(System.in);
     //ATTRIBUTES: variable initialization
 
     //orders input
@@ -12,5 +15,36 @@ public class Orditf {
     LinkedList<String> items = new LinkedList<>();
     LinkedList<Integer> prices = new LinkedList<>();
 
-    
+    //main ordering method
+    public void getOrder(){
+        boolean cicada = true;
+                //prompt only
+        System.out.println("Input orders: 1-" + prices.size());
+        while(cicada){
+            System.out.print("order: ");
+            int usOrder = (sc.nextInt()-1);
+            sc.nextLine();
+            orders.add(usOrder);
+            
+            System.out.print("add order? Y/N: ");
+            String addDecision = sc.nextLine().toUpperCase();
+            switch (addDecision) {
+                case "Y":
+                    cicada = true;
+                    break;
+                case "N":
+                    cicada = false;
+                    break;
+                default:
+                    throw new InputMismatchException();
+            }
+        }
+    }
+
+    public void displayOrder(){
+        for(int i = 0; i < orders.size(); i++){
+            int refVar = orders.get(i);
+            System.out.println(items.get(refVar) + "---" + prices.get(refVar));
+        }
+    }
 }
